57b6006cc13002954d4fc89d3c1f73e088e18b65
Nmudang <60430427+Nmudang@users.noreply.github.com>
2021-04-17 14:05:00 +0700

    Add files via upload

---
+++ userdb.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2021 at 09:05 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdb`
+--
+
+CREATE TABLE `userdb` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2 NOT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2 NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2 NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2 NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_thai_520_w2 NOT NULL,
+  `ass_notify` tinyint(1) NOT NULL DEFAULT 0,
+  `notify` int(100) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userdb`
+--
+
+INSERT INTO `userdb` (`id`, `firstname`, `lastname`, `email`, `password`, `username`, `ass_notify`, `notify`) VALUES
+(1, 'mingyu', 'Kim', 'mingyu97@svt.com', '$2y$10$W/IxCowr/c44yKrzLUYCe.ewvEngk79S99CwBTcUg3QXKFKWdSTi.', 'mingyu97', 0, 0),
+(2, 'กนกกร', 'นามเปรมปรีดิ์', '6110613301@student.tu.ac.th', '$2y$10$7asTsrcCkIuyzPAJy9Vh5uFLOILL9FdJlPrXro3QgYnBpr7RIOg.W', 'mudang', 0, 0),
+(5, 'eiei', 'ieie', 'eee@mail.com', '$2y$10$4H518/5JiCsJgnNNBojMt.Ql0l/6o5PAfBtKuKtWiVIQ6JvFXHD4u', 'eee', 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userdb`
+--
+ALTER TABLE `userdb`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userdb`
+--
+ALTER TABLE `userdb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
